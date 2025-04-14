af11f2c26777ff38b0efde76e2573985026fc2f9
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-04-14 14:12:24 +0300

    addad assignment

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+ function About(props){
+    const dImage = "https://via.placeholder.com/215";
+    return(
+        <aside>
+            <img src = {dImage} alt="blog logo"/>
+            <p>{props.about}</p>
+        </aside>
+    );
+ }
+ export default About;
