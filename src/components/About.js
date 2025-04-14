8c18dd5f84cf0001345981122c1f6b7e9625b788
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-04-14 14:36:34 +0300

    added assignments

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
- function About(props){
-    const dImage = "https://via.placeholder.com/215";
-    return(
-        <aside>
-            <img src = {dImage} alt="blog logo"/>
-            <p>{props.about}</p>
-        </aside>
-    );
- }
- export default About;
+
+function About(props) {
+  const dImage = "https://via.placeholder.com/215";
+  
+  return (
+    <aside>
+      <img src={props.image || dImage} alt="blog logo" />
+      <p>{props.about}</p>
+    </aside>
+  );
+}
+
+export default About;
